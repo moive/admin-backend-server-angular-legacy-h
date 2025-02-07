8207f39a6bd31af55596b91c9162f095b2681475
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2025-02-06 23:23:16 -0500

    add express validator

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { createUser, getUsers } from "../controllers/user.controller";
+import { check } from "express-validator";
 
 const router = Router();
 
 router.get("/", getUsers);
-router.post("/", createUser);
+router.post(
+	"/",
+	[
+		check("name", "The name is required.").not().isEmpty(),
+		check("password", "The password is required.").not().isEmpty(),
+		check("email", "The email is required.").isEmail(),
+	],
+	createUser
+);
 
 export default router;
