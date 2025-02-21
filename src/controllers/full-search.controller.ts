5066fb64552e7cee9c0f79441b0e5cff2e0cd99e
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2025-02-21 13:40:15 -0500

    full search logic code

--- src/controllers/full-search.controller.ts
+++ src/controllers/full-search.controller.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from "express";
+import { User } from "../models/user.model";
+import { Hospital } from "../models/hospital.model";
+import { Doctor } from "../models/doctor.model";
 
 const getFullSearch = async (req: Request, res: Response) => {
 	const search = req.params.search;
+	const name = new RegExp(search, "i");
+
 	try {
-		res.json({ ok: true, search });
+		const [user, hospital, doctor] = await Promise.all([
+			User.find({ name }, "name email"),
+			Hospital.find({ name }, "name"),
+			Doctor.find({ name }, "name email"),
+		]);
+
+		res.json({ ok: true, user, hospital, doctor });
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({
