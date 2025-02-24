2a3e757dd1bc30892982e5af332c8e749ddffcf5
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2025-02-23 21:38:21 -0500

    search for specific collection

--- src/controllers/full-search.controller.ts
+++ src/controllers/full-search.controller.ts
@@ -24,4 +24,43 @@ const getFullSearch = async (req: Request, res: Response) => {
 	}
 };
 
-export { getFullSearch };
+const getCollectionFullSearch = async (req: Request, res: Response) => {
+	const search = req.params.search;
+	const table = req.params.table;
+	const name = new RegExp(search, "i");
+	let data = [];
+	try {
+		switch (table) {
+			case "doctors":
+				data = await Doctor.find({ name })
+					.populate("user", "name img")
+					.populate("hospital", "name img");
+				break;
+			case "hospitals":
+				data = await Hospital.find({ name }).populate("user", "name img");
+				break;
+			case "users":
+				data = await User.find({ name });
+				break;
+
+			default:
+				return res.status(400).json({
+					ok: false,
+					msg: "Table not found",
+				});
+		}
+
+		res.json({
+			ok: false,
+			results: data,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: "Please talk to the administrator",
+		});
+	}
+};
+
+export { getFullSearch, getCollectionFullSearch };
--- src/routes/full-search.routes.ts
+++ src/routes/full-search.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { validarJWT } from "../middlewares/validator-jwt.middleware";
-import { getFullSearch } from "../controllers/full-search.controller";
+import {
+	getCollectionFullSearch,
+	getFullSearch,
+} from "../controllers/full-search.controller";
 
 const router = Router();
 
 router.get("/:search", [validarJWT], getFullSearch);
+router.get("/collection/:table/:search", [validarJWT], getCollectionFullSearch);
 
 export default router;
