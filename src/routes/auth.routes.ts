7310e1fc106dff7d888e2edbf332d1b42c2d251d
Moises Velasquez <moises007@gmail.com>
2026-02-05 23:13:34 -0500

    chore(google): changes files

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import {
-	googleSignIn,
-	login,
-	renewToken,
+  googleSignIn,
+  login,
+  renewToken,
 } from "../controllers/auth.controller";
 import { check } from "express-validator";
 import { validatorFields } from "../middlewares/validator-fields.middleware";
@@ -10,24 +10,27 @@ import { validateJWT } from "../middlewares/validator-jwt.middleware";
 
 const router = Router();
 
+// https://developers.google.com/identity/gsi/web/guides/overview
+// https://developers.google.com/identity/gsi/web/guides/display-button?hl=es-419#javascript
+
 router.post(
-	"/",
-	[
-		check("email", "The email is not valid").isEmail(),
-		check("email", "The email is required").not().isEmpty(),
-		check("password", "The password is required").not().isEmpty(),
-		validatorFields,
-	],
-	login
+  "/",
+  [
+    check("email", "The email is not valid").isEmail(),
+    check("email", "The email is required").not().isEmpty(),
+    check("password", "The password is required").not().isEmpty(),
+    validatorFields,
+  ],
+  login,
 );
 
 router.post(
-	"/google",
-	[
-		check("token", "The google token is required").not().isEmpty(),
-		validatorFields,
-	],
-	googleSignIn
+  "/google",
+  [
+    check("token", "The google token is required").not().isEmpty(),
+    validatorFields,
+  ],
+  googleSignIn,
 );
 
 router.get("/renew", validateJWT, renewToken);
